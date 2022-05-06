e047c1493b74cdcb7c0b376180502da090b07225
Kris Faber <hello@krisfaber.com>
2022-05-06 10:23:25 +0530

    use execCommand

--- src/fill-email.ts
+++ src/fill-email.ts
@@ -1,14 +1,27 @@
 import { generateEmail } from './utils/generate-email';
 import { GenerationHistory } from './utils/history';
 
+function replaceValue(el: HTMLInputElement, value: string) {
+  if (el) {
+    el.focus();
+    el.select();
+    if (!document.execCommand('insertText', false, value)) {
+      // Fallback for Firefox: just replace the value
+      el.value = value;
+    }
+    el.dispatchEvent(new Event('change', { bubbles: true })); // usually not needed
+  }
+
+  return el;
+}
+
 (async () => {
   const email = await generateEmail();
   const generationHistory = new GenerationHistory();
   const activeTextarea = document.activeElement;
 
   if (activeTextarea) {
-    (activeTextarea as HTMLInputElement).value = email;
-    (activeTextarea as HTMLInputElement).dispatchEvent(new Event('change'));
+    replaceValue(activeTextarea as HTMLInputElement, email);
     await generationHistory.append(email);
   }
 })();
